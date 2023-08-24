94a4111d8c1101d1fd4b3f444970ae461669973c
Josefina Lagos <josefinalagos289@gmail.com>
2023-08-24 10:53:56 -0300

    Cree una carpeta llamada COMPONENTS y en esta cree componentes llamados, INICIO, LOGIN, NAVBAR, PRODUCTOS, REGISTRO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { ProductosComponent } from './components/productos/productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InicioComponent,
+    NavbarComponent,
+    RegistroComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,
