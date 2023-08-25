76b87ce1bf1a310511f88b358e2ed9d2a5ca1ef8
Josefina Lagos <josefinalagos289@gmail.com>
2023-08-25 08:56:15 -0300

    Adentro de la carpeta Modules cree una carpeta  Auth y dentro de la misma cree componentes llamados:lOGIN,REGISTER y una carpeta llamada Sevices

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { InicioComponent } from './components/inicio/inicio.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { RegistroComponent } from './components/registro/registro.component';
-import { ProductosComponent } from './components/productos/productos.component';
-import { RegisterComponent } from './modules/pages/register/register.component';
-import { ButtonComponent } from './shared/components/button/button.component';
+
+
+
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { LoginComponent } from './modules/auth/login/login.component';
+import { RegisterComponent } from './modules/auth/register/register.component';
+import { InicioComponent } from './modules/inicio/inicio.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    InicioComponent,
-    NavbarComponent,
-    RegistroComponent,
-    ProductosComponent,
-    RegisterComponent,
-    ButtonComponent,
     FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    InicioComponent,
   ],
   imports: [
     BrowserModule,
