cc9b64845413ba89eb8c6bda94bc157945473676
Melani050 <tina.meli25@gmail.com>
2023-09-13 09:29:30 -0300

    creacion de rutas para comunicacion entre componentes dentro de inicio

--- src/app/modules/inicio/inicio-routing.module.ts
+++ src/app/modules/inicio/inicio-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 const routes: Routes = [
-  {path: '', component: InicioComponent}
+  {path: 'inicio', component: InicioComponent},
+  {path: 'cards', component: CardsComponent}
+
 ];
 
 @NgModule({
