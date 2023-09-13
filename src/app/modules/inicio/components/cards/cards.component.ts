3bd3c6e43536255f21a1a81d5dfe59c9575c6992
Melani050 <tina.meli25@gmail.com>
2023-09-13 11:44:04 -0300

    creo una propiedad y la inicializo

--- src/app/modules/inicio/components/cards/cards.component.ts
+++ src/app/modules/inicio/components/cards/cards.component.ts
@@ -9,8 +9,23 @@ import { Mascota } from 'src/app/models/mascotas';
   styleUrls: ['./cards.component.css'],
 })
 export class CardsComponent {
+  // PROPIEDAD PÚBLICA (TIPO ARRAY)
+  public info: Mascota[];
 
-  constructor(){}
+  constructor(){
+    // INICIALIZA LA PROPIEDAD INFO
+    this.info = [
+      {
+        id: '',
+        nombre: 'Julian',
+        sexo: 'Macho',
+        tamanio: 'Mediano',
+        raza: 'Siames',
+        imagen: 'https://www.purina.es/sites/default/files/styles/ttt_image_510/public/2021-01/Siamese%201.jpg?itok=dS6uQvHe',
+        alt: 'Gato siames'
+      }
+    ]
+  }
 
   ngOnInit(): void{
   }
