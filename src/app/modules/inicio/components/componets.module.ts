80b7b4e029687d5aa00da2b8d4d30bdfce8e934b
Melani050 <tina.meli25@gmail.com>
2023-11-03 10:59:37 -0300

    carousel base para modificarlo

--- src/app/modules/inicio/components/componets.module.ts
+++ src/app/modules/inicio/components/componets.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CarouselComponent } from './carousel/carousel.component';
 
 
 
 @NgModule({
   declarations: [
-    CarouselComponent
   ],
   imports: [
     CommonModule
