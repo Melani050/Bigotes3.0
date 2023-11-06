0eb51bfea6231a3f82d80ce199584bb93d8ce89c
Melani050 <tina.meli25@gmail.com>
2023-11-06 20:43:35 -0300

    cree el componente apartados

---
+++ src/app/modules/inicio/components/apartados/apartados.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apartados',
+  templateUrl: './apartados.component.html',
+  styleUrls: ['./apartados.component.css']
+})
+export class ApartadosComponent {
+
+}
--- src/app/modules/inicio/components/componets.module.ts
+++ src/app/modules/inicio/components/componets.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ApartadosComponent } from './apartados/apartados.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ApartadosComponent
+  ],
   imports: [
     CommonModule
   ]
