f77f8389af0ffe24c3a970acd6570978f1343368
Melani050 <tina.meli25@gmail.com>
2023-11-06 20:57:52 -0300

    agregue un componente de angular material para modificarlo

--- src/app/modules/inicio/components/apartados/apartados.component.ts
+++ src/app/modules/inicio/components/apartados/apartados.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
+
 @Component({
   selector: 'app-apartados',
   templateUrl: './apartados.component.html',
   styleUrls: ['./apartados.component.css']
 })
 export class ApartadosComponent {
-
+  tiles: Tile[] = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
 }
--- src/app/modules/inicio/inicio.module.ts
+++ src/app/modules/inicio/inicio.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InicioComponent } from './inicio/inicio.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { ApartadosComponent } from './components/apartados/apartados.component';
 
+/** ANGULAR MATERIAL */
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import {NgFor} from '@angular/common';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+
+
 import { InicioRoutingModule } from './inicio-routing.module';
-import { ApartadosComponent } from './components/apartados/apartados.component';
+import { MatGridList } from '@angular/material/grid-list';
+
 
 @NgModule({
   declarations: [
@@ -20,11 +28,13 @@ import { ApartadosComponent } from './components/apartados/apartados.component';
     MatButtonModule,
     MatCardModule,
     InicioRoutingModule,
-    MatIconModule
+    MatIconModule,
+    MatGridListModule
   ],
   exports: [
     MatButtonModule,
     MatCardModule,
+    MatGridListModule,
     // Hay que exportar el componente para que el inicio pueda utilizarlo
     CardsComponent,
     ApartadosComponent
