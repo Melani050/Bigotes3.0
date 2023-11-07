bba68041b7ef1e00ebca4974907ab9402ccfa740
Melani050 <tina.meli25@gmail.com>
2023-11-06 21:28:22 -0300

    elimine el componente de angular

--- src/app/modules/inicio/components/apartados/apartados.component.ts
+++ src/app/modules/inicio/components/apartados/apartados.component.ts
@@ -1,11 +1,5 @@
 import { Component } from '@angular/core';
 
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
-}
 
 
 @Component({
@@ -14,10 +8,5 @@ export interface Tile {
   styleUrls: ['./apartados.component.css']
 })
 export class ApartadosComponent {
-  tiles: Tile[] = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
-  ];
+
 }
