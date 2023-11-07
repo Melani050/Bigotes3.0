f3f892d418e0048c23b800172e21e8c334d9ba52
Melani050 <tina.meli25@gmail.com>
2023-11-07 09:40:15 -0300

    coloque la ruta de admin

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './pages/admin/admin.component';
 
 const routes: Routes = [
   {
