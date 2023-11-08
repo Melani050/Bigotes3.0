5aa92ed04de1335152cf294b94369e96a472b3ff
Melani050 <tina.meli25@gmail.com>
2023-11-08 10:59:06 -0300

    cree el formulario base hay que modificarlo y agregar estilo

--- src/app/modules/admin/components/table/table.component.ts
+++ src/app/modules/admin/components/table/table.component.ts
@@ -9,5 +9,23 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent {
+  coleccionMascotas: Mascota[] = []; // creamos colección basada en interfaz Mascota
+
+  mascotaSeleccionada!: Mascota; // ! -> recibir valores vacios
+
+  // formulario vinculado al archivo HTML
+  mascota = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    sexo: new FormControl('', Validators.required),
+    tamanio: new FormControl('', Validators.required),
+    raza: new FormControl('', Validators.required),
+    imagen: new FormControl('', Validators.required),
+    alt: new FormControl('', Validators.required),
+    personalidad: new FormControl('', Validators.required),
+    edad: new FormControl('', Validators.required),
+    peso: new FormControl('', Validators.required),
+    castrado: new FormControl('', Validators.required),
+    desparasitado: new FormControl('', Validators.required),
+  })
 
 }
