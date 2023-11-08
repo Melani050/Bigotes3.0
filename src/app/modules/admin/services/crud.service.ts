bc93a34ed68b266fa317e84461a83dd6adf11fa1
Melani050 <tina.meli25@gmail.com>
2023-11-08 09:34:31 -0300

    importe lo que necesita el crud service

--- src/app/modules/admin/services/crud.service.ts
+++ src/app/modules/admin/services/crud.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Mascota } from 'src/app/models/mascotas';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { map } from 'rxjs/operators'; // mapea valores
 
 @Injectable({
   providedIn: 'root'
