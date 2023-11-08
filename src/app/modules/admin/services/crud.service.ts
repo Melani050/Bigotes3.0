c3bcdb024216a3dd2fb85976ddf4765bb8221332
Melani050 <tina.meli25@gmail.com>
2023-11-08 10:17:45 -0300

    cree una coleccion local y la referencio con la coleccion de la base de datos

--- src/app/modules/admin/services/crud.service.ts
+++ src/app/modules/admin/services/crud.service.ts
@@ -7,6 +7,21 @@ import { map } from 'rxjs/operators'; // mapea valores
   providedIn: 'root'
 })
 export class CrudService {
+  // Creo la colección "mascotasCollection" basandome el la colección "AngularFirestoreCollection"
+  // y respetando la interfaz de Mascota.
+  private mascotasCollection: AngularFirestoreCollection<Mascota>
 
-  constructor() { }
+  // Defino de forma local "AngularFirestore" como database.
+  constructor(private database: AngularFirestore) {
+    // Referencio la colección mascotas de la base de datos con mi colección local. 
+    this.mascotasCollection = database.collection('mascotas')
+  }
+
+  // CRUD
+  crearMascota(){}
+  obtenerMasco(){}
+  modificarMascota(){}
+  eliminarMascota(){}
+
+
 }
