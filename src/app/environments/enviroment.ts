880950b66ee0155869e67b15b6acc9ee24793384
Melani050 <tina.meli25@gmail.com>
2023-11-09 11:05:47 -0300

    por si era un error

--- src/app/environments/enviroment.ts
+++ src/app/environments/enviroment.ts
@@ -1,14 +1,14 @@
 //DESARROLLO
-export const environment={
-  production:false,
-   //SACAMOS EL cons, CAMBIAMOS EL "=" POR ":" Y SACAMOS EL ";"
-  firebaseConfig : {
-    apiKey: "AIzaSyCk90Im6jdoc6BWst4UsrP8jJ3vZmBgZtQ",
-    authDomain: "bigotes30-fd737.firebaseapp.com",
-    projectId: "bigotes30-fd737",
-    storageBucket: "bigotes30-fd737.appspot.com",
-    messagingSenderId: "866053511091",
-    appId: "1:866053511091:web:b1767b06a557d2cf18ddc6",
-    measurementId: "G-XF5KMT7V14"
-  }
-}
+export const environment = {
+  production: false,
+  //SACAMOS EL cons, CAMBIAMOS EL "=" POR ":" Y SACAMOS EL ";"
+  firebaseConfig: {
+    apiKey: 'AIzaSyCk90Im6jdoc6BWst4UsrP8jJ3vZmBgZtQ',
+    authDomain: 'bigotes30-fd737.firebaseapp.com',
+    projectId: 'bigotes30-fd737',
+    storageBucket: 'bigotes30-fd737.appspot.com',
+    messagingSenderId: '866053511091',
+    appId: '1:866053511091:web:b1767b06a557d2cf18ddc6',
+    measurementId: 'G-XF5KMT7V14',
+  },
+};
