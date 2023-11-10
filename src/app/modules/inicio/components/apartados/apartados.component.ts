7bb7cabf8325231abea543e429abe4f7c8799cf6
Melani050 <tina.meli25@gmail.com>
2023-11-10 10:26:01 -0300

    agregue información

--- src/app/modules/inicio/components/apartados/apartados.component.ts
+++ src/app/modules/inicio/components/apartados/apartados.component.ts
@@ -1,32 +1,5 @@
 import { Component } from '@angular/core';
-import {NestedTreeControl} from '@angular/cdk/tree';
-import { MatTreeNestedDataSource } from '@angular/material/tree';
 
-
-interface FoodNode {
-  name: string;
-  children?: FoodNode[];
-}
-
-const TREE_DATA: FoodNode[] = [
-  {
-    name: 'Fruit',
-    children: [{name: 'Apple'}, {name: 'Banana'}, {name: 'Fruit loops'}],
-  },
-  {
-    name: 'Vegetables',
-    children: [
-      {
-        name: 'Green',
-        children: [{name: 'Broccoli'}, {name: 'Brussels sprouts'}],
-      },
-      {
-        name: 'Orange',
-        children: [{name: 'Pumpkins'}, {name: 'Carrots'}],
-      },
-    ],
-  },
-];
 
 @Component({
   selector: 'app-apartados',
@@ -34,12 +7,8 @@ const TREE_DATA: FoodNode[] = [
   styleUrls: ['./apartados.component.css']
 })
 export class ApartadosComponent {
-  treeControl = new NestedTreeControl<FoodNode>(node => node.children);
-  dataSource = new MatTreeNestedDataSource<FoodNode>();
 
   constructor() {
-    this.dataSource.data = TREE_DATA;
+   
   }
-
-  hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
 }
