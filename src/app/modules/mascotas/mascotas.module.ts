1a3732f80ecb56ae905c25c8c0e9ad9a5834c054
Melani050 <tina.meli25@gmail.com>
2023-11-20 14:56:29 -0300

    cree los componentes de perro, gato y mascotas

--- src/app/modules/mascotas/mascotas.module.ts
+++ src/app/modules/mascotas/mascotas.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MascotasRoutingModule } from './mascotas-routing.module';
+import { MascotasComponent } from './pages/mascotas/mascotas.component';
+import { PerrosComponent } from './pages/perros/perros.component';
+import { GatosComponent } from './pages/gatos/gatos.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MascotasComponent,
+    PerrosComponent,
+    GatosComponent
+  ],
   imports: [
     CommonModule,
     MascotasRoutingModule
