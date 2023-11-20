0bce9f1c44ba82c2957c6cc45a0c9bdb31ecfd73
Melani050 <tina.meli25@gmail.com>
2023-11-20 14:51:43 -0300

    cree el modulo de mascotas

---
+++ src/app/modules/mascotas/mascotas.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MascotasRoutingModule } from './mascotas-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MascotasRoutingModule
+  ]
+})
+export class MascotasModule { }
