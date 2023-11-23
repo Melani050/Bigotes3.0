0166c86dd5e835bd8d1053f356b4459a1aedf190
juanforman <juanchiforman32@gmail.com>
2023-11-23 09:01:41 -0300

    empezando crud usuario

---
+++ src/app/modules/usuario/pages/usuariofinal/usuario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FirestoreService } from 'src/app/shared/services/firestore.service';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './usuario.component.html',
+  styleUrls: ['./usuario.component.css']
+})
+export class TableComponent {
+
+}
