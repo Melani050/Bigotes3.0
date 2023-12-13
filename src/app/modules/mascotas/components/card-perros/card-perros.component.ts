71ed3867fa0452be831df64706e82477d4ce0c38
melani050 <tina.meli25@gmail.com>
2023-12-13 17:25:33 -0300

    agregue el filtro de perro

--- src/app/modules/mascotas/components/card-perros/card-perros.component.ts
+++ src/app/modules/mascotas/components/card-perros/card-perros.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Mascota } from 'src/app/models/mascotas';
+import { CrudService } from 'src/app/modules/admin/services/crud.service';
 
 @Component({
   selector: 'app-card-perros',
@@ -6,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-perros.component.css']
 })
 export class CardPerrosComponent {
+  coleccionMascotas: Mascota[] = []; // todas las mascotas
 
+  perros: Mascota[] = []; // mascotas especie = collares
+
+  mascotaSeleccionada!: Mascota;
+
+  modalVisible: boolean = false;
+
+  constructor(
+    public servicioCrud: CrudService
+  ){}
+
+  ngOnInit(): void{
+    this.servicioCrud.obtenerMascota().subscribe(mascota =>{
+      this.coleccionMascotas = mascota;
+
+      this.mostrarPerros();
+    })
+  }
+
+  mostrarVer(info: Mascota){
+    // al seleccionar "ver más" el modal visible pasa a "true"
+    this.modalVisible = true;
+    // muestra la información de la mascota que se seleccione
+    this.mascotaSeleccionada = info;
+  }
+
+  mostrarPerros(){
+    // forEach itera la colección/ arreglo
+    this.coleccionMascotas.forEach(mascota =>{
+      /* si la categoría de mascota es igual a "perros"
+        se va a enviar a la colección "perros"*/
+      if(mascota.especie === "perro"){
+        this.perros.push(mascota);
+      }
+    })
+  }
 }
